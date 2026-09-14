7d8a0a0faf4d1d94dfe94a5f25b48e55db9da03b
Sam Sclaver <sclaversam@gmail.com>
2026-09-14 15:18:31 -0400

    Add all 11 CMU 2025 game URLs

--- data-raw/cmu_2025_games.R
+++ data-raw/cmu_2025_games.R
@@ -1,7 +1,18 @@
 # Carnegie Mellon 2025 game boxscore URLs (no ?view= suffix).
 # Fill in the remaining 10 from CMU's d3football schedule:
 #   https://www.d3football.com/teams/Carnegie_Mellon/2025/index
+# Carnegie Mellon 2025 game boxscore URLs (no ?view= suffix).
 cmu_2025_games <- c(
-  "https://www.d3football.com/seasons/2025/boxscores/20250906_e064.xml"  # at Chicago
-  # add the rest here
+  "https://www.d3football.com/seasons/2025/boxscores/20250906_e064.xml", # Sep 6
+  "https://www.d3football.com/seasons/2025/boxscores/20250913_6i95.xml", # Sep 13
+  "https://www.d3football.com/seasons/2025/boxscores/20250920_l58t.xml", # Sep 20
+  "https://www.d3football.com/seasons/2025/boxscores/20251004_gwn0.xml", # Oct 4
+  "https://www.d3football.com/seasons/2025/boxscores/20251011_t8bp.xml", # Oct 11
+  "https://www.d3football.com/seasons/2025/boxscores/20251018_uxv5.xml", # Oct 18
+  "https://www.d3football.com/seasons/2025/boxscores/20251025_3c40.xml", # Oct 25
+  "https://www.d3football.com/seasons/2025/boxscores/20251101_dlys.xml", # Nov 1
+  "https://www.d3football.com/seasons/2025/boxscores/20251108_3ubg.xml", # Nov 8
+  "https://www.d3football.com/seasons/2025/boxscores/20251115_h3wh.xml", # Nov 15
+  "https://www.d3football.com/seasons/2025/boxscores/20251122_xsab.xml"  # Nov 22
 )
+ 
